54dff735a9379ad17fd7db1b84822966e6f6ba37
Allison Higginbotham <higgintop@gmail.com>
2015-02-04 12:39:33 -0600

    get rid of merge conflict

--- app/js/main.js
+++ app/js/main.js
@@ -6,19 +6,13 @@
     });
 
 
-<<<<<<< HEAD
 
-  function populator(){
 
-    console.log("button clicked");
-  }
 
-=======
     function populator () {
     console.log("button clicked");
     
     }
->>>>>>> 5e99658c729051ff40e40ff0050a6a1b32812bab
 })();
 
 
